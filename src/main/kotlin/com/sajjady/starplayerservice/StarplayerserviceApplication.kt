6e1d4ee29287cfa22c87875bcf30a3dec30d8d3b
sajjadyousefnia <sdyussuf@gmail.com>
2020-03-04 17:29:37 +0330

    - first commit.

---
+++ src/main/kotlin/com/sajjady/starplayerservice/StarplayerserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.sajjady.starplayerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarplayerserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarplayerserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sajjady/starplayerservice/StarplayerserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sajjady.starplayerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarplayerserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
