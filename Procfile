c1aefd0a5b5b6a0dca3d34cf8db2d8a0ff6df021
sajjadyousefnia <sdyussuf@gmail.com>
2020-03-25 18:39:00 +0430

    - specified java version and removed Procfile and other its settings in build.gradle.kts.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar build/server/webapp-runner-*.jar build/libs/*.war
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
     war
+    java
+    application
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
     kotlin("plugin.jpa") version "1.3.61"
@@ -88,6 +90,7 @@ tasks.withType<KotlinCompile> {
     delete(rootProject.buildDir)
 }*/
 
+/*
 task("stage") {
     dependsOn("clean", war)
 }
@@ -101,6 +104,7 @@ tasks.register("copyToLib", Copy::class.java) {
     }
 }
 tasks.findByName("stage")!!.dependsOn("copyToLib")
+*/
 
 /*tasks["stage"].doLast {
     delete {
