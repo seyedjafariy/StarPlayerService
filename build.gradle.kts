cd59af739299f8d2592177d19f827a2b908d6605
sajjadyousefnia <sdyussuf@gmail.com>
2020-03-25 18:43:36 +0430

    - specified java version and removed Procfile and other its settings in build.gradle.kts 2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
     war
-    java
-    application
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
     kotlin("plugin.jpa") version "1.3.61"
